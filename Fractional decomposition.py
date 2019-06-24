2343aab799fb0a0760ff31769fecfc9de1d17862
강병안 <aosldk95@naver.com>
2019-06-24 11:40:28 +0900

    Fractional

--- Fractional decomposition.py
+++ Fractional decomposition.py
@@ -1,13 +1,16 @@
 #13195의 소인수는 5, 7, 13, 29 입니다.
 #600851475143 의 최대 소인수는 무엇입니까?
-number = 13195
+number = 600851475143
 numberlist = []
 for i in range(2,number) :
     if (number % i) == 0 :
         numberlist.append(i)
+        print(numberlist)
 for i in numberlist :
     for j in range(2,i):
         if (i % j) == 0 :
-            del numberlist[numberlist.index(i)]
+            print(1)
+            numberlist[numberlist.index(i)] = 0
+            break
 print(max(numberlist))
             
